fc6e15064bda33cdbb7b23aa761077cfe0008bd9
rbn42 <bl100@students.waikato.ac.nz>
2016-06-14 01:27:37 +1200

    submit

--- main.py
+++ main.py
@@ -4,25 +4,41 @@
 Hello
 
 Usage:
-  main.py <configuration_file> get <path> <name> 
+  main.py <configuration_file> get <path> <name>
   main.py <configuration_file> set <path> <name> <value>
+  main.py <configuration_file> submit
 
 Options:
   -h --help     Show this screen.
 """
 import configparser
+import shutil
+import os.path
 from docopt import docopt
 arguments = docopt(__doc__)
 file_path = arguments['<configuration_file>']
+beforesubmit = file_path + '.beforesubmit'
 path = arguments['<path>']
 name = arguments['<name>']
 value = arguments['<value>']
 config = configparser.ConfigParser()
-config.read(file_path)
 
 if arguments['get']:
+    config.read(file_path)
     print(config[path][name])
+
 elif arguments['set']:
+    if os.path.exists(beforesubmit):
+        config.read(beforesubmit)
+    else:
+        config.read(file_path)
     config[path][name] = value
-    with open(file_path, 'w') as configfile:
+    with open(beforesubmit, 'w') as configfile:
         config.write(configfile)
+
+elif arguments['submit']:
+    if os.path.exists(beforesubmit):
+        config.read(beforesubmit)
+        os.system('cat %s > %s; rm %s;' %
+                  (beforesubmit, file_path, beforesubmit))
+        #shutil.move(beforesubmit, file_path)
